457c89bd624379244489d9b64b8ff1824055e675
benspector-mls <ben@marcylabschool.org>
2024-10-09 09:49:31 -0400

    moving tests

--- tests/from-scratch.spec.js
+++ tests/from-scratch.spec.js
@@ -11,13 +11,13 @@ const {
   listAllKeys,
   listAllValues,
   convertToMatrix,
-} = require('./from-scratch');
+} = require('../src/from-scratch');
 
 const testSuiteName = 'From Scratch Tests';
 const scoresDir = path.join(__dirname, '..', 'scores');
 const scoreCounter = new ScoreCounter(testSuiteName, scoresDir);
 
-const log = jest.spyOn(console, 'log').mockImplementation(() => {});
+const log = jest.spyOn(console, 'log').mockImplementation(() => { });
 
 describe(testSuiteName, () => {
   afterEach(jest.clearAllMocks);
